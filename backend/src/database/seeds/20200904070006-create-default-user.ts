bad48dd2dda6ab9eb27a79e7fce2ec17066f92f9
Araujoz22 <120515121+Araujoz22@users.noreply.github.com>
2024-12-17 12:22:27 -0300

    full 2

--- backend/src/database/seeds/20200904070006-create-default-user.ts
+++ backend/src/database/seeds/20200904070006-create-default-user.ts
@@ -4,14 +4,14 @@ import { hash } from "bcryptjs";
 module.exports = {
   up: (queryInterface: QueryInterface) => {
     return queryInterface.sequelize.transaction(async t => {
-      const passwordHash = await hash("123456", 8);
+      const passwordHash = await hash("523523698741", 8);
       return Promise.all([
         queryInterface.bulkInsert(
           "Users",
           [
             {
-              name: "Admin",
-              email: "admin@admin.com",
+              name: "Warllyson",
+              email: "vwarllyson@gmail.com",
               profile: "admin",
               passwordHash,
               companyId: 1,
